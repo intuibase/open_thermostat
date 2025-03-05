c64c63b7a5a274a0254d0a7e0959112fc1f7e8b2
Pawel Filipczak <pawel.filipczak@elastic.co>
2025-03-05 18:55:07 +0100

    OTA updates

--- src/REST.h
+++ src/REST.h
@@ -1,6 +1,6 @@
 #pragma once
 
-
+#include <esp_ota_ops.h>
 #include <SPIFFS.h>
 #include <WebServer.h>
 #include <uri/UriBraces.h>
@@ -16,6 +16,7 @@
 namespace heating {
 
 using namespace std::string_view_literals;
+using namespace std::string_literals;
 
 class WebServerStringView : public WebServer {
 public:
@@ -37,6 +38,10 @@ public:
 		}
 	}
 
+	void sendBytes(const char *bytes, size_t size) { _currentClientWrite(bytes, size); }
+	void endResponse() { _finalizeResponse(); }
+
+	HTTPUpload *getUpload() { return _currentUpload.get(); }
 };
 
 class REST {
@@ -61,6 +66,7 @@ public:
 		server_.on("/status/rooms", [this]() { roomsStatus(); });
 		server_.on("/status/devices", [this]() { devicesFound(); });
 		server_.on("/status/programs", HTTP_GET, [this]() { showPrograms(); }); // show available programs
+		server_.on("/status/version", [this]() { version(); });
 		server_.on("/params/boiler", [this]() { emsParams(); });
 
 
@@ -75,6 +81,9 @@ public:
 
 		server_.on("/config/program/current", [this]() { configProgramCurrent(); }); // get selected program
 		server_.on("/config/reboot", HTTP_GET, [this]() { configReboot(); });
+
+		server_.on("/ota", HTTP_POST, [this]() { handleOTAResponse(); }, [this]() { handleOTAUpdate(); });
+		server_.on("/otafs", HTTP_POST, [this]() { handleOTAResponse(); }, [this]() { handleOTAFFSUpdate(); });
 
 		server_.on("/", HTTP_GET, [this]() { index(); });
 
@@ -248,6 +257,28 @@ private:
 		ib::viewable_stringbuf payloadBuf;
 		std::ostream payload(&payloadBuf);
 		controller_.getFullStatus(payload);
+
+		server_.sendView(200, "application/json"sv, payloadBuf.view());
+	}
+
+	void version() {
+		DBGLOGREST("version\n");
+
+		const esp_app_desc_t *app = esp_ota_get_app_description();
+
+		ib::viewable_stringbuf payloadBuf;
+		std::ostream payload(&payloadBuf);
+		payload << '{';
+		payload << "\"project\": \""sv << app->project_name << "\","sv;
+		payload << "\"version\": \""sv << app->version << "\","sv;
+		payload << "\"date\": \""sv << app->date << "\","sv;
+		payload << "\"time\": \""sv << app->time << "\","sv;
+		payload << "\"idf\": \""sv << app->idf_ver << "\","sv;
+		payload << "\"sha256\": \""sv;
+		for (size_t i = 0; i < sizeof(app->app_elf_sha256); ++i) {
+			payload << std::hex << std::setw(2) << std::setfill('0') << (int)app->app_elf_sha256[i];
+		}
+		payload << "\"}";
 
 		server_.sendView(200, "application/json"sv, payloadBuf.view());
 	}
@@ -658,9 +689,195 @@ private:
 		} else {
 			return {true, "text/plain"};
 		}
-
-
 	}
+
+	void handleOTAUpdate() {
+		HTTPUpload &upload = server_.upload();
+
+		if (upload.status == UPLOAD_FILE_START) {
+			auto size = server_.arg("size");
+			long fileSize = atol(size.c_str());
+
+			DBGLOGREST("handleOTA START '%s', totalSize: '%zu'\n", upload.filename.c_str(), fileSize);
+
+			ota_ = OTAUpload{};
+
+			ota_.partition = esp_ota_get_next_update_partition(NULL);
+			if (!ota_.partition) {
+				DBGLOGREST("OTA partition not found\n");
+				ota_.errorMessage = "OTA partition not found"sv;
+				ota_.error = -1;
+				return;
+			}
+
+			if (fileSize > 0 && fileSize > ota_.partition->size) {
+				DBGLOGREST("handleOTAFFSUpdate Partition size %zu smaller than binary file %zu!\n", ota_.partition->size, fileSize);
+				ota_.errorMessage = "Partition smaller than file"sv;
+				ota_.error = -1;
+				return;
+			}
+
+
+			DBGLOGREST("handleOTA Found partition '%s', size: %d, encrypted: %d\n", ota_.partition->label, ota_.partition->size, ota_.partition->encrypted);
+
+			DBGLOGREST("Beginning OTA\n");
+
+			if (esp_ota_mark_app_valid_cancel_rollback() != ESP_OK) {
+				DBGLOGREST("Beginning OTA: esp_ota_mark_app_valid_cancel_rollback failed\n");
+			}
+
+			ota_.error = esp_ota_begin(ota_.partition, OTA_SIZE_UNKNOWN, &ota_.handle);
+			if (ota_.error != ESP_OK) {
+				DBGLOGREST("Beginning OTA failed!\n");
+				return;
+			}
+			ota_.started = true;
+
+			DBGLOGREST("Beginning OTA handle: %d\n", ota_.handle);
+		} else if (upload.status == UPLOAD_FILE_WRITE) {
+			if (!ota_.started || ota_.error != ESP_OK) {
+				if (!ota_.writeErrorReported) {
+					DBGLOGREST("handleOTA writing skipped, OTA error: %d\n", ota_.error);
+					ota_.writeErrorReported = true;
+				}
+				return;
+			}
+
+			DBGLOGREST("handleOTA writing to OTA handle %d, size: %zu\n", ota_.handle, upload.currentSize);
+
+			ota_.error = esp_ota_write(ota_.handle, upload.buf, upload.currentSize);
+			if (ota_.error != ESP_OK) {
+				DBGLOGREST("handleOTA writing to OTA handle %d, size: %zu FAILED, error: %d\n", ota_.handle, upload.currentSize, ota_.error);
+				esp_ota_abort(ota_.handle);
+				return;
+			}
+		} else if (upload.status == UPLOAD_FILE_END) {
+			if (!ota_.started || ota_.error != ESP_OK) {
+				DBGLOGREST("handleOTA upload end, error: %d\n", ota_.error);
+				return;
+			}
+
+			DBGLOGREST("handleOTA ending OTA\n");
+
+			ota_.error = esp_ota_end(ota_.handle);
+			if (ota_.error != ESP_OK) {
+				DBGLOGREST("handleOTA finalizing OTA handle %d, FAILED, error: %d\n", ota_.handle, ota_.error);
+				return;
+			}
+
+			DBGLOGREST("handleOTA setting boot partition\n");
+
+			ota_.error = esp_ota_set_boot_partition(ota_.partition);
+			if (ota_.error != ESP_OK) {
+				DBGLOGREST("handleOTA setting boot partition FAILED, error: %d\n", ota_.error);
+				return;
+			}
+			ota_.success = true;
+
+		} else if (upload.status == UPLOAD_FILE_ABORTED) {
+			if (!ota_.started) {
+				DBGLOGREST("handleOTA upload aborted, OTA not started. Error: %d\n", ota_.error);
+				return;
+			}
+			DBGLOGREST("handleOTA ABORTED\n");
+			esp_ota_abort(ota_.handle);
+		}
+	}
+
+	void handleOTAResponse() {
+		auto ctype = "text/plain"sv;
+		if (ota_.success) {
+			server_.sendView(200, ctype, "success"sv);
+		} else {
+			ib::viewable_stringbuf payloadBuf;
+			std::ostream ss(&payloadBuf);
+			ss << "Failure: ";
+			ss << ota_.errorMessage << " (" << ota_.error << ')';
+			server_.sendView(500, ctype, payloadBuf.view());
+		}
+	}
+
+	void handleOTAFFSUpdate() {
+		HTTPUpload &upload = server_.upload();
+
+		if (upload.status == UPLOAD_FILE_START) {
+			auto size = server_.arg("size");
+			long fileSize = atol(size.c_str());
+
+			DBGLOGREST("handleOTAFFSUpdate START '%s', totalSize: '%zu'\n", upload.filename.c_str(), fileSize);
+
+			ota_ = OTAUpload{};
+			ota_.partition = esp_partition_find_first(ESP_PARTITION_TYPE_DATA, ESP_PARTITION_SUBTYPE_DATA_SPIFFS, nullptr);
+			if (!ota_.partition) {
+				DBGLOGREST("handleOTAFFSUpdate partition not found\n");
+				ota_.errorMessage = "FS partition not found"sv;
+				ota_.error = -1;
+				return;
+			}
+
+			DBGLOGREST("handleOTAFFSUpdate Found partition '%s', size: %d, encrypted: %d\n", ota_.partition->label, ota_.partition->size, ota_.partition->encrypted);
+
+			if (fileSize > 0 && fileSize > ota_.partition->size) {
+				DBGLOGREST("handleOTAFFSUpdate Partition size %zu smaller than binary file %zu!\n", ota_.partition->size, fileSize);
+				ota_.errorMessage = "FS partition smaller than file"sv;
+				ota_.error = -1;
+				return;
+			}
+
+			SPIFFS.end();
+
+			DBGLOGREST("handleOTAFFSUpdate Erasing partition\n");
+			ota_.error = esp_partition_erase_range(ota_.partition, 0, ota_.partition->size);
+			if (ota_.error != ESP_OK) {
+				ota_.errorMessage = "FS partition erase failure"sv;
+				DBGLOGREST("handleOTAFFSUpdate Failed to erase SPIFFS partition!\n");
+				return;
+			}
+
+			ota_.started = true;
+		} else if (upload.status == UPLOAD_FILE_WRITE) {
+			if (!ota_.started || ota_.error != ESP_OK) {
+				if (!ota_.writeErrorReported) {
+					DBGLOGREST("handleOTAFFSUpdate writing skipped, OTA error: %d\n", ota_.error);
+					ota_.writeErrorReported = true;
+				}
+				return;
+			}
+			DBGLOGREST("handleOTAFFSUpdate writing SPIFFS offset: %zu, size: %zu\n", ota_.offset, upload.currentSize);
+
+			ota_.error = esp_partition_write(ota_.partition, ota_.offset, upload.buf, upload.currentSize);
+			if (ota_.error != ESP_OK) {
+				ota_.errorMessage = "FS partition write error"sv;
+				DBGLOGREST("handleOTAFFSUpdate Failed to write SPIFFS partition: %d\n", ota_.error);
+				return;
+			}
+			ota_.offset += upload.currentSize;
+		} else if (upload.status == UPLOAD_FILE_END) {
+			if (!ota_.started || ota_.error != ESP_OK) {
+				DBGLOGREST("handleOTAFFSUpdate upload end, error: %d\n", ota_.error);
+				return;
+			}
+			DBGLOGREST("handleOTAFFSUpdate finished\n");
+			ota_.success = true;
+		} else if (upload.status == UPLOAD_FILE_ABORTED) {
+			if (!ota_.started) {
+				DBGLOGREST("handleOTAFFSUpdate upload aborted. Not started. Error: %d\n", ota_.error);
+				return;
+			}
+			DBGLOGREST("handleOTAFFSUpdate ABORTED\n");
+		}
+	}
+
+	struct OTAUpload {
+		bool started = false;
+		bool success = false;
+		const esp_partition_t *partition = nullptr;
+		size_t offset = 0;
+		esp_ota_handle_t handle = 0;
+		esp_err_t error = ESP_OK;
+		std::string errorMessage;
+		bool writeErrorReported = false;
+	} ota_;
 
 	HeatingController &controller_;
 	WebServerStringView server_;
